264c2f39d939aa107ab559c3c7f670835f2a7974
Abhishek-Gharat <www.gharatabhi50@gmail.com>
2024-07-19 17:04:48 +0530

    style: Adjust menu icon position and size in Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Header.module.css';
 import Logo from '../assets/img_arrow_up.svg'; // Adjust path to your SVG
-import { IoMenu } from "react-icons/io5";
+import { IoMenu } from 'react-icons/io5';
+import { CiLogin } from 'react-icons/ci';
 
 const Header = () => {
+  // State to manage mobile menu visibility
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Toggle menu visibility
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
@@ -11,12 +20,12 @@ const Header = () => {
           <img src={Logo} alt="Tribzy Logo" className={styles.logoImage} />
           <span className={styles.logoText}>Tribzy</span>
         </div>
-        {/* Render the menu icon without click functionality */}
-        <div className={styles.menuIcon}>
-          <IoMenu size={30} className={styles.menuIcon} />
+        {/* Menu Icon for mobile view */}
+        <div className={styles.menuIcon} onClick={toggleMenu}>
+          <IoMenu size={30} />
         </div>
-        {/* Render navigation links or menu based on showMenu state */}
-        <nav className={styles.nav}>
+        {/* Render navigation links or menu based on isMenuOpen state */}
+        <nav className={`${styles.nav} ${isMenuOpen ? styles.navOpen : ''}`}>
           <a href="#" className={styles.navLink}>Home</a>
           <a href="#" className={styles.navLink}>Communities</a>
           <a href="#" className={styles.navLink}>About Us</a>
